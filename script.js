ada3dfb551d3e11c202d47f4f06f8807849228ed
chadnelson101 <chadnelson279@gmail.com>
2023-11-13 13:16:27 -0800

    new commit

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-let button = document.getElementById('btn')
+let button = document.getElementById('btn');
 
-button.addEventListener('click', () =>{
+button.addEventListener('click', () => {
     const height = parseInt(document.getElementById('height').value);
-    const weight = parseFloat(document.getElementById('weight').value);
+    const weight = parseInt(document.getElementById('weight').value);
     const result = document.getElementById('output');
-    let height_status=false, weight_status=false
+    let height_status=false, weight_status=false;
 
-    if(height === '' || isNaN(height <= 0)){
-        document.getElementById('height_error').innerHTML= 'Please provide a valid height'
+    if(height === '' || isNaN(height) || (height <= 0)){
+        document.getElementById('height_error').innerHTML = 'Please provide a valid height';
     }else{
-        document.getElementById('height_error').innerHTML = '';height_status=true
+        document.getElementById('height_error').innerHTML = '';
+        height_status=true;
     }
 
-    if(wieght === '' || isNaN(wieght) || (wieght <= 0)){
+    if(weight === '' || isNaN(weight) || (weight <= 0)){
         document.getElementById('weight_error').innerHTML = 'Please provide a valid weight';
     }else{
-        document.getElementById('weight_error').innerHTML = '';weight_status=true
+        document.getElementById('weight_error').innerHTML = '';
+        weight_status=true;
     }
 
-   if(height_status && weight_status){
-    const bmi = (weight / ((height*height)/1000)).toFixed(2);
+    if(height_status && weight_status){
+        const bmi = (weight / ((height*height)/10000)).toFixed(2);
 
-    if(bmi < 18.5){
-        result.innerHTML = 'under weight :' + bmi;
-    }else if(bmi >= 18.5 && bmi < 24.9){
-        result.innerHTML = 'healthy range :' + bmi
+        if(bmi < 18.6){
+            result.innerHTML = 'Under weight : ' + bmi;
+        }else if(bmi >= 18.6 && bmi < 24.9){
+            result.innerHTML = 'Normal : ' + bmi;
+        }else{
+            result.innerHTML = 'Over weight : ' + bmi;
+        }
     }else{
-        result.innerHTML = 'overweight :' + bmi;
+        alert('The form has errors');
+        result.innerHTML = '';
     }
-   }else{
-    alert('The form has errors');
-    result.innerHTML = '';
-   }
 });
